541e6b9925ef6f161d02730ffdb5ddcb59c467c0
Mr Phone <sales@mrphonelb.com>
2025-10-20 17:35:59 +0300

    Initial commit - Mastercard backend

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/initiate-checkout", async (req, res) => {
     console.log(`💰 Creating session for ${amount} ${currency} | Draft: ${draftId}`);
 
     const payload = {
-      apiOperation: "CREATE_CHECKOUT_SESSION",
+      apiOperation: "INITIATE_CHECKOUT",
       interaction: {
         operation: "PURCHASE",
         returnUrl: "https://www.mrphonelb.com/client/contents/checkout",
